59e50b7f9f55191deb702d78e1a9dba315c26f4b
Bobby Lei <bobl53@gmail.com>
2018-07-14 13:03:46 +0800

    Upto

--- CSP/Part2/polling/src/polling/core.clj
+++ CSP/Part2/polling/src/polling/core.clj
@@ -1,6 +1,24 @@
-(ns polling.core)
+(ns polling.core
+  (:require [clojure.core.async :as async :refer :all
+              :exclude [map into reduce merge partition partition-by take]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Problem: Parking calls need to be made directly within a go block. Clojure's
+; macro system wont be able to perform its magic otherwise.
+; Hence, inside the action function, you cannot use <! or >!
+(defn poll-fn [interval action]
+  (let [seconds (* interval 1000)]
+    (go (while true
+          (action)
+          (<! (timeout seconds))))))
+; (poll-fn 10 #(println "Polling at:" (System/currentTimeMillis)))
+
+; Solution
+(defmacro poll [interval & body]
+  `(let [seconds# (* ~interval 1000)]
+      (go (while true
+            (do ~@body)
+            (<! (timeout seconds#))))))
+
+; (poll 10
+;   (println "Polling at:" (System/currentTimeMillis))
+;   (println (<! ch)))
